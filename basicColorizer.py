a979132943c27f8f855588a25fdf13e84312562f
Harsh K <52982332+Koomawat@users.noreply.github.com>
2021-05-05 14:55:58 -0400

    Update basicColorizer.py

--- basicColorizer.py
+++ basicColorizer.py
@@ -65,14 +65,25 @@ def basic(img, img2, k, rgbDataPoints, height, width):
     indexList = []
     representativeColors = []
 
+    locations = []
+
+    thousandSamples = []
+
+    for i in range(heightCut):
+        for j in range(widthCut):
+
+            if i !=0 and i != heightCut-1 and j !=0 and j != widthCut-1:
+                locations.append((i,j))
+
+    thousandSamples = random.sample(locations,1000)
+    
 
     for h in range (heightCut):
         print(h)
         for w in range (widthCut):
 
             if h!=0 and h!= heightCut-1 and w!= widthCut-1:
-                
-                # Surrounding pixels + middle pixel
+
                 g0 = picGray[h-1, w-1+widthCut]
                 g1 = picGray[h-1, w+widthCut]
                 g2 = picGray[h-1, w+1+widthCut]   
@@ -83,47 +94,45 @@ def basic(img, img2, k, rgbDataPoints, height, width):
                 g7 = picGray[h+1, w+widthCut]
                 g8 = picGray[h+1, w+1+widthCut]
             
-                
-                
-                for h2 in range (heightCut):
-                    for w2 in range (widthCut):
-                        
-                        if h2!=0 and h2!= heightCut-1 and w2!=0 and w2!= widthCut-1:
 
-                            g_0 = bwTraining[h2-1, w2-1]
-                            g_1 = bwTraining[h2-1, w2]
-                            g_2 = bwTraining[h2-1, w2+1]   
-                            g_3 = bwTraining[h2, w2-1]
-                            g_4 = bwTraining[h2, w2]
-                            g_5 = bwTraining[h2, w2+1]
-                            g_6 = bwTraining[h2+1, w2-1]
-                            g_7 = bwTraining[h2+1, w2]
-                            g_8 = bwTraining[h2+1, w2+1]
+                for i in range (1000):
 
-                            # calculate Euclidean distance 
-                            eucDistance = math.sqrt(
-                            (abs(g0 - g_0) ** 2) + 
-                            (abs(g1 - g_1) ** 2) +
-                            (abs(g2 - g_2) ** 2) +
-                            (abs(g3 - g_3) ** 2) +
-                            (abs(g4 - g_4) ** 2) +
-                            (abs(g5 - g_5) ** 2) +
-                            (abs(g6 - g_6) ** 2) +
-                            (abs(g7 - g_7) ** 2) +
-                            (abs(g8 - g_8) ** 2))
-                            #print(eucDistance)
-                            euclideanListCompare.append(eucDistance)
-                            indexList.append((h2,w2))  
+                    h2 = thousandSamples[i][0]
+                    w2 = thousandSamples[i][1]
+
+                    g_0 = bwTraining[h2-1, w2-1]
+                    g_1 = bwTraining[h2-1, w2]
+                    g_2 = bwTraining[h2-1, w2+1]   
+                    g_3 = bwTraining[h2, w2-1]
+                    g_4 = bwTraining[h2, w2]
+                    g_5 = bwTraining[h2, w2+1]
+                    g_6 = bwTraining[h2+1, w2-1]
+                    g_7 = bwTraining[h2+1, w2]
+                    g_8 = bwTraining[h2+1, w2+1]
+
+                    eucDistance = 0
+
+                    eucDistance = math.sqrt(
+                    ((g0 - g_0) ** 2) + 
+                    ((g1 - g_1) ** 2) +
+                    ((g2 - g_2) ** 2) +
+                    ((g3 - g_3) ** 2) +
+                    ((g4 - g_4) ** 2) +
+                    ((g5 - g_5) ** 2) +
+                    ((g6 - g_6) ** 2) +
+                    ((g7 - g_7) ** 2) +
+                    ((g8 - g_8) ** 2))
+                    #print(eucDistance)
+                    euclideanListCompare.append((eucDistance))
+                    indexList.append((h2,w2))  
 
                 #print("-------------------")
                 #print(euclideanListCompare)
 
-                # six similar seraches
                 for i in range(6):
                     
                     #print("------------------")
                     minEuc = min(enumerate(euclideanListCompare), key=itemgetter(1))[0]
-                    val = min(enumerate(euclideanListCompare), key=itemgetter(1))[1]
                     #print(minEuc, " ",val)
                     location = indexList[minEuc]
                     locationH = location[0]
@@ -132,7 +141,8 @@ def basic(img, img2, k, rgbDataPoints, height, width):
                     representativeColors.append(currColor)
                     del euclideanListCompare[minEuc]
                     del indexList[minEuc]
-            
+
+                
                 colorCount = collections.Counter(representativeColors)
                 commonColor = colorCount.most_common()
                 #print(commonColor)
@@ -159,7 +169,7 @@ def basic(img, img2, k, rgbDataPoints, height, width):
 
                 pixels[w+widthCut, h] = (0,0,0)
 
-    halfKMeansCopy.show()
+    #halfKMeansCopy.show()
     plt.imshow(halfKMeansCopy, cmap='gray')
     plt.show()
     print("done")
